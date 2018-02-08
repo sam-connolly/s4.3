7be1d19755384c02684db89f08b21102cc62ea12
sam-connolly <samwi5e@hotmail.co.uk>
2018-02-08 10:17:11 +0000

    It now self destructs

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,19 @@ int main()
     int j=0;
     lcd.cls();
     lcd.locate(0,0);
-    lcd.printf("mbed application shield!");
-    lcd.locate(0,10);
-    lcd.printf("char %dx%d : %dx%d pixels",
-               lcd.columns(), lcd.rows(),
-               lcd.width(), lcd.height() );
-    lcd.circle(100, 20, 10, 1);
+    lcd.printf("Unrecognised system");
 
-    while(true) {
-        lcd.locate(0,20);
-        lcd.printf("Counting : %4d",j);
-        j++;
+    lcd.locate(0,10);
+    lcd.printf("Self destructing in:");
+    int i = 10;
+    while(i >= 0)
+    {
+      lcd.locate(90,10);
+        lcd.printf("%4d",i);
+        i--;
         wait(1.0);
     }
+
+    lcd.locate(0,20);
+    lcd.printf("***BOOM***");
 }
